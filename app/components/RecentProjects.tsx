192a525f716755f2978c9a1d255679249790d072
Aaryan Prabhakar <aaryanprabhakar07@gmail.com>
2024-05-30 11:10:53 +0530

    added navigation pill

--- app/components/RecentProjects.tsx
+++ app/components/RecentProjects.tsx
@@ -5,7 +5,7 @@ import { FaLocationArrow } from "react-icons/fa";
 
 const RecentProjects = () => {
   return (
-    <div className="py-20">
+    <div className="py-20" id="projects">
       <h1 className="heading">
         A small selection of{" "}
         <span className="text-purple">recent projects</span>
@@ -42,6 +42,7 @@ const RecentProjects = () => {
         </div>
 
     </div>
+    
 </PinContainer>
 
 
